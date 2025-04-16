37f544f9be4219f29aa059fd0511816e54d2d8a7
coderpy04 <ritesh.2201141ec@iiitbh.ac.in>
2025-04-17 01:05:35 +0530

    Setting up Vapi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
+
+
+
 const HomePage = () => {
   return (
     <div>
       HomePage
+
+      <SignedIn>
+          <SignOutButton />
+      </SignedIn>
+
+      <SignedOut>
+          <SignInButton />
+      </SignedOut>
+
     </div>
   )
 }
